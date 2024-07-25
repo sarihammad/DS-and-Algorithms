2ad991e7ff9df92162b5a2d4a39f9cd0fee56a66
Sari Hammad <sari_hammad@hotmail.com>
2024-07-25 15:49:47 -0400

    added problem

---
+++ Graph/Topological Sort/alien_dictionary.py
@@ -0,0 +1,42 @@
+class Solution:
+  def alienOrder(self, words: List[str]) -> str:
+    # create a graph: node for each letter, 
+    # edge for each known ordering (same prefix, different letters at curr index)
+    # return topological sort of graph
+
+    # create adj list
+    graph = {char: [] for word in words for char in word}
+    for i in range(len(words) - 1):
+      first = words[i]
+      second = words[i+1]
+      min_length = min(len(first), len(second))
+      for j in range(min_length):
+        if first[j] != second[j]:
+          graph[first[j]].append(second[j])
+          break
+
+    visited = {char: 0 for char in graph} # 0: unvisited, 1: visiting, 2: visited
+    answer = []
+
+    # dfs
+    def dfs_get_topological_sort(char):
+        if visited[char] == 1:
+            return
+        if visited[char] == 2:
+            return
+        
+        visited[char] = 1
+        for neighbor in graph[char]:
+            dfs_get_topological_sort(neighbor)
+
+        visited[char] = 2
+        answer.insert(0, char) 
+
+    # dfs each unvisited node to get the topological sort
+    for char in graph:
+      if visited[char] == 0:
+          dfs_get_topological_sort(char)
+
+    answer = "".join(answer)
+
+    return answer
