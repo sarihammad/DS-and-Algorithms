05c1797a5d7b1109db6505711b0db5ff93a25203
Sari Hammad <sari_hammad@hotmail.com>
2024-07-28 23:16:46 -0400

    update solution

--- Dynamic Programming/edit_distance.py
+++ Dynamic Programming/edit_distance.py
@@ -1,6 +1,6 @@
 class Solution:
-    # bottom up
     def minDistance(self, word1: str, word2: str) -> int:
+        # bottom up
         m, n = len(word1), len(word2)
         dp = [[0] * (n + 1) for i in range(m + 1)]
 
@@ -19,3 +19,23 @@ class Solution:
                                    dp[i - 1][j - 1] + 1)  # substitute
 
         return dp[m][n]
+
+class Solution:
+    # top down
+    def minDistance(self, word1: str, word2: str) -> int:
+        def dp(i, j, memo={}):
+            if i == 0:
+                return j
+            if j == 0:
+                return i
+            if word1[i - 1] == word2[j - 1]:
+                return dp(i - 1, j - 1)
+            return min(dp(i - 1, j, memo) + 1,
+                dp(i, j - 1, memo) + 1,
+                dp(i - 1, j - 1, memo) + 1)
+        m, n = len(word1), len(word2)
+        return dp(m, n)
+
+        
+        
+
