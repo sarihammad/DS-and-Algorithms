7636bc8dfc5b09f1af9eab745aca25b7d7e4d8a7
Sari Hammad <sari_hammad@hotmail.com>
2025-06-28 10:50:49 -0400

    Valid Palindrome

---
+++ Two Pointers/Valid Palindrome/ValidPalindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if (s == null) return false;
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            // Step 1: Skip non-alphanumeric characters
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            // Step 2: Compare current letters ignoring case
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
