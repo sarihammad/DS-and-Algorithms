2eb2a9094bec75efbbcf5725889a47d809d91426
Sari Hammad <sari_hammad@hotmail.com>
2025-07-11 12:20:51 -0400

    3Sum

--- Two Pointers/3Sum/3_sum.cpp
+++ Two Pointers/3Sum/3_sum.cpp
@@ -1,30 +1,27 @@
-// Sort the array, iterate over first value i, two pointers just after i and at the end of the list to balance the sum to 0. Skip duplicates once a solution is found.
 class 3Sum {
 public:
+    // sort, for each num init left and right ptrs moving to balance sum to 0.
+    // avoid duplicates by skipping already added nums and left and right elements.
     vector<vector<int>> threeSum(vector<int>& nums) {
-        std::sort(nums.begin(), nums.end());
-        std::vector<std::vector<int>> result;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            int left = i + 1;
-            int right = nums.size() - 1;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            if (i > 0 && nums[i - 1] == nums[i]) continue;
+            int left = i + 1, right = n - 1;
             while (left < right) {
-                int total = nums[i] + nums[left] + nums[right];
-                if (total < 0) {
-                    left++;
-                } else if (total > 0) {
-                    right--;
-                } else {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
                     result.push_back({nums[i], nums[left], nums[right]});
                     left++;
                     right--;
-
-                    // just added a duplicate, make sure next left and right are different
                     while (left < right && nums[left] == nums[left - 1]) left++;
                     while (left < right && nums[right] == nums[right + 1]) right--;
-                }
+                } else if (sum < 0) left++;
+                else right--;
             }
         }
+
         return result;
     }
 };
